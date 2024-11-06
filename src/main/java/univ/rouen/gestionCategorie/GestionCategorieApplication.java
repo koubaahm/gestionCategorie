f90469da0540aced7bb5d19da27ba0f47747121e
koubaahm <ahmed.koubaa@univ-rouen.fr>
2024-11-06 16:07:55 +0100

    commit initial

---
+++ src/main/java/univ/rouen/gestionCategorie/GestionCategorieApplication.java
@@ -0,0 +1,13 @@
+package univ.rouen.gestionCategorie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCategorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCategorieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univ/rouen/gestionCategorie/GestionCategorieApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.rouen.gestionCategorie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCategorieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
